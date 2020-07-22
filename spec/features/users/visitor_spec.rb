e7674d75b0dbb74636caa7e46db15aeebea15b74
Sienna Kopf <princess.kopf@gmail.com>
2020-07-21 20:12:02 -0600

    Add incomplete user story 6

--- spec/features/users/visitor_spec.rb
+++ spec/features/users/visitor_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe "as a default user" do
   describe "I try to view pages starting with /merchant, /admin, /profile" do
     it "displays a 404 error" do
-      user = User.create(name: "Quin", role: 0)
+      visitor = User.create(name: "Quin", role: 0)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(visitor)
 
       visit "/merchant/dashboard"
+      save_and_open_page
       expect(page).to have_content("The page you were looking for doesn't exist.")
 
       visit "/admin/users"
