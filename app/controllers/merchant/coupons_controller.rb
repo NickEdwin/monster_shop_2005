8d43bd36fe10046418a020df065028c38caf5bc7
Nick Edwin <nickedwin85@gmail.com>
2020-08-01 10:49:39 -0600

    Create method for coupon with strong params

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -1,4 +1,22 @@
 class Merchant::CouponsController < Merchant::BaseController
+  def new
+    @item = Item.find(params[:item_id])
+  end
+
   def create
+    coupon = Coupon.create(coupon_params)
+    if coupon.save
+      flash[:success] = "Your coupon has been created"
+      redirect_to "/merchant/items"
+    else
+      flash[:error] = coupon.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:name, :min_items, :discount, :item_id, :merchant_id)
   end
 end
